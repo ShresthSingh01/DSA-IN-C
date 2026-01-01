f6d05377b9c2f76887286584871384c3aba6c92a
SHRESTH SINGH <shresthsingh112233@gmail.com>
2026-01-01 20:12:54 +0530

    Array
    Array Operation

---
+++ Arrays/ArrayInsertion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {10, 20, 30, 40, 50};
+    int n = 5, pos = 2, val = 25;
+
+    for(int i = n; i > pos; i--)
+        arr[i] = arr[i-1];
+
+    arr[pos] = val;
+    n++;
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
